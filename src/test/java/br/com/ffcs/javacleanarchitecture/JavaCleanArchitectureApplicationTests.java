8cc6fca9d32825cf41cc84df4a2dacc73286783c
Fagner Fernandes <ffcs@cin.ufpe.br>
2023-06-26 09:15:00 -0300

    Initial commit

---
+++ src/test/java/br/com/ffcs/javacleanarchitecture/JavaCleanArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ffcs.javacleanarchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCleanArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
